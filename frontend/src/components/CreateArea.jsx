96ac5b1e1810057441a253679bc9469631013f3e
“GowthamG30” <gowtham.gorrepati30@gmail.com>
2024-06-10 19:03:00 +0530

    Add empty title validation

--- frontend/src/components/CreateArea.jsx
+++ frontend/src/components/CreateArea.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 const CreateArea = (props) => {
+	const [emptyTitle, setEmptyTitle] = useState(false);
+
 	const [note, setNote] = useState({
 		title: "",
 		content: "",
 	});
 
-	const handleChnage = (event) => {
+	const handleChange = (event) => {
 		const { name, value } = event.target;
 
 		setNote((prevNote) => {
@@ -18,11 +20,20 @@ const CreateArea = (props) => {
 	};
 
 	const submitNote = (event) => {
-		props.onAdd(note);
-		setNote({
-			title: "",
-			content: "",
-		});
+		if(note.title === "") {
+			setEmptyTitle(true);
+			setTimeout(() => {
+				setEmptyTitle(false);
+			}, 2000);
+		}
+		else {
+			props.onAdd(note);
+			setNote({
+				title: "",
+				content: "",
+			});
+		}
+		
 		event.preventDefault(); // prevent refresh
 	};
 
@@ -31,19 +42,29 @@ const CreateArea = (props) => {
 			<form>
 				<input
 					name="title"
-					onChange={handleChnage}
+					onChange={handleChange}
 					value={note.title}
 					placeholder="Title"
 				/>
 				<textarea
 					name="content"
-					onChange={handleChnage}
+					onChange={handleChange}
 					value={note.content}
 					placeholder="Take a note..."
 					rows="3"
 				/>
 				<button onClick={submitNote}>Add</button>
 			</form>
+			{
+				emptyTitle ?
+					<div className="alert alert-warning" role="alert">
+						Title cannot be empty!
+					</div>
+				:
+				<div className="alert alert-warning empty-alert" role="alert">
+					&nbsp;
+				</div>
+			}
 		</div>
 	);
 };
