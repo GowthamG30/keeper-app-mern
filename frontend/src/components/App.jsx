819eb3697ac38b7e4cb55c64f8f3e4dfb2cf16ef
“GowthamG30” <gowtham.gorrepati30@gmail.com>
2024-06-11 12:26:30 +0530

    Add filter by status feature

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -7,6 +8,7 @@ import axios from "axios";
 
 const App = () => {
 	const [Tasks, setTasks] = useState([]);
+	const [statusFilter, setStatusFilter] = useState("All");
 
 	useEffect(() => {
 		axios
@@ -73,7 +75,17 @@ const App = () => {
 		<div>
 			<Header />
 			<CreateArea onAdd={addNote} />
-			{Tasks.map((task) => {
+			<DropdownButton
+				id="dropdown-basic-button"
+				title={statusFilter}
+				onSelect={(eventKey) => setStatusFilter(eventKey)}
+			>
+				<Dropdown.Item eventKey="All">All</Dropdown.Item>
+				<Dropdown.Item eventKey="To Do">To Do</Dropdown.Item>
+				<Dropdown.Item eventKey="In Progress">In Progress</Dropdown.Item>
+				<Dropdown.Item eventKey="Done">Done</Dropdown.Item>
+			</DropdownButton>
+			{Tasks.filter((task) => (statusFilter === "All" || task.noteStatus === statusFilter)).map((task) => {
 				return (
 					<Note
 						key={task._id}
